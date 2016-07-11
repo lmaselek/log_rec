fae536e2a769c95aa3dafeab3e080c10189e5023
lmaselek <lmaselek@gmail.com>
2016-07-11 20:03:06 +0200

    init commit

---
+++ include/log_record.hrl
@@ -0,0 +1,13 @@
+%%%-------------------------------------------------------------------
+%%% @author lmaselek
+%%% @copyright (C) 2014, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 17. lip 2014 19:59
+%%%-------------------------------------------------------------------
+-author("lmaselek").
+
+-record(rec, {a1, a2, a3, a4, nazwa_jeden, nazwa_dwa}).
+
+-record(inny, {nazwa_b, nazwa_c, nazwa_d}).
